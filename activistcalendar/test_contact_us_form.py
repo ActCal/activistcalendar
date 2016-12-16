c1cb6cb317882762f0c7f5b4a2cc4254fd3ed7f8
Daniel J Raine <ley.danielj@gmail.com>
2016-12-15 20:48:32 -0500

    Remove commented out alternate tests
    Discard several formats of tests that did not work.

--- activistcalendar/test_contact_us_form.py
+++ activistcalendar/test_contact_us_form.py
@@ -1,10 +1,12 @@
 """
-This suite tests the contact_us route, covering the OutsideContact model.
+This suite tests the contact_us and the contact_submit routes
+Tests should use the OutsideContact model.
 """
 
 from django.test import TestCase
 from django.test import Client
 from activistcalendar.models import OutsideContact
+
 
 class OutsideContactTestCase(TestCase):
     def setUp(self):
@@ -24,37 +26,13 @@ class OutsideContactTestCase(TestCase):
         )
 
     def test_form_submitted(self):
+        '''POST to /contact_submit route from the contact_us.html template'''
         client = Client()
         response = client.post('/contact_submit/',
-                               {
-                                    'input_contact_name': 'David Test',
-                                    'input_contact_email': 'david314@gmail.com',
-                                    'input_contact_subject': 'Subject field test',
-                                    'input_contact_message': 'Hello from message field'
-                               })
+            {
+                'input_contact_name': 'David Test',
+                'input_contact_email': 'david314@gmail.com',
+                'input_contact_subject': 'Subject field test',
+                'input_contact_message': 'Hello from message field'
+            })
         self.assertEquals(response.status_code, 200)
-
-        # # Alternatively, using the OutsideContact model
-        # client = Client()
-        # david = OutsideContact.objects.get(contact_name="David Test")
-        # response = client.post('/contact_submit/', david)
-        # self.assertEquals(response.status_code, 201)
-
-    # def test_form_is_filled(self):
-    #     '''Form fields contain outside contact data'''
-    #     david = OutsideContact.objects.get(contact_name="David Test")
-    #     self.assertEqual(david.contact_id, "1")
-    #     self.assertEqual(david.contact_name, "David Test")
-    #     self.assertEqual(david.contact_email, "david314@gmail.com")
-    #     self.assertEqual(david.contact_subject, "Subject field test")
-    #     self.assertEqual(david.contact_message, "Hello from the message field")
-
-    # def test_form_submit(self):
-    #     '''Form is submitted successfully'''
-    #     def test_form(self):
-    #         form = MyForm()
-    #         self.assertTrue(form)
-    #         self.assertTrue(form.is_valid())
-
-    # def tearDown(self):
-    #     pass
