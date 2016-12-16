60b930a903c539067d8245dfbe526cc69f27ca11
Daniel J Raine <ley.danielj@gmail.com>
2016-12-16 10:03:52 -0500

    Add assertions to check saving contact to database
    Add assertions to test_form_submitted using query to database to retrieve OutsideContact

--- activistcalendar/test_contact_us_form.py
+++ activistcalendar/test_contact_us_form.py
@@ -10,14 +10,6 @@ from activistcalendar.models import OutsideContact
 
 class OutsideContactTestCase(TestCase):
     def setUp(self):
-
-        OutsideContact.objects.create(
-            contact_name="David Test",
-            contact_email="david314@gmail.com",
-            contact_subject="Subject field test",
-            contact_message="Hello from the message field"
-        )
-
         OutsideContact.objects.create(
             contact_name="Donald Test",
             contact_email="donald159@gmail.com",
@@ -27,12 +19,18 @@ class OutsideContactTestCase(TestCase):
 
     def test_form_submitted(self):
         '''POST to /contact_submit route from the contact_us.html template'''
-        client = Client()
-        response = client.post('/contact_submit/',
+        self.client = Client()
+        response = self.client.post('/contact_submit/',
             {
                 'input_contact_name': 'David Test',
                 'input_contact_email': 'david314@gmail.com',
                 'input_contact_subject': 'Subject field test',
                 'input_contact_message': 'Hello from message field'
             })
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
+
+        contact = OutsideContact.objects.get(contact_name="David Test")
+        self.assertEqual(contact.contact_name, "David Test")
+        self.assertEqual(contact.contact_email, "david314@gmail.com")
+        self.assertEqual(contact.contact_subject, "Subject field test")
+        self.assertEqual(contact.contact_message, "Hello from message field")
